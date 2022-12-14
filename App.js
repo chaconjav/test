886041626923169471efd569c95593836c67b10f
Javier <chaconjav@yahoo.com>
2022-12-13 21:13:35 -0600

    Primer Commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola MUNDO</Text>
+      <Text>Segunda linea</Text>
+    </View>
+  );
+}
+
+export default App;
